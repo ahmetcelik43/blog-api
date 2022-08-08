b416e417cefed2a00e37b6fc6d2afa52fe6fca54
ahmetcelik43 <ahmet.celik5443@gmail.com>
2022-08-08 20:24:42 +0300

    ok

--- app/server.js
+++ app/server.js
@@ -32,7 +32,7 @@ app.get("/fake-pass", async(request, response, next) => {
     })
     //exports.app = functions.https.onRequest(app);
 app.listen(port,"https://blog-api43.herokuapp.com",() => {
-    console.log("Sunucu Başarılı bir şekilde çalışıyor..." + " port:" + PORT);
+    console.log("Sunucu Başarılı bir şekilde çalışıyor..." + " port:" + port);
 });
 // const mysql = require('mysql');
 // const webPush = require("web-push");
