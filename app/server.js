73c11bd5efc4e53f72be12ed3ec5c1c6425fbde2
ahmetcelik43 <ahmet.celik5443@gmail.com>
2022-08-08 20:10:16 +0300

    ok

--- app/server.js
+++ app/server.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcrypt');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-const PORT =80;
+const PORT =443;
 //const functions = require("firebase-functions");
 const config=require('./config')
 
