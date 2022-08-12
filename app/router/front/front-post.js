173a21f4f73f2f1d72532d97f5f84b4b3bc4173d
ahmetcelik43 <ahmet.celik5443@gmail.com>
2022-08-13 00:28:29 +0300

    ok

--- app/router/front/front-post.js
+++ app/router/front/front-post.js
@@ -34,9 +34,40 @@ router.get("/getAll", (request, response, next) => {
 })
 
 router.get("/get", (request, response, next) => {
-    const dao = db.getInstance();
+    const dao = db.getInstance()
     dao.all('SELECT * FROM cats', []).then((data) => {
         response.json(data)
+    })
+})
+
+router.get("/getBySlug", (request, response, next) => {
+    const dao = db.getInstance();
+    const { slug } = request.query
+    const query = 'SELECT  posts.* \n' +
+        ', cats.id as catid \n' +
+        'FROM posts \n' +
+        'inner join cats on cats.id=posts.cat \n' +
+        ' where posts.slug=?'
+
+    dao.get(query, [slug]).then(async(data) => {
+        let g = data.map(async(i) => {
+            const split = i.tags.split(',')
+            let qr = []
+            split.forEach((i, index) => {
+                qr.push(" id=?")
+            })
+            let obj = i
+            const q = "select id,nametr,nameen from tags where " + qr.join(' or ')
+            await dao.all(q,
+                split).then((d) => {
+                obj["tagler"] = d
+            })
+            return obj
+        })
+
+        Promise.all(g).then((d) => {
+            response.json(d)
+        })
     })
 })
 
--- app/router/post.js
+++ app/router/post.js
@@ -5,9 +5,9 @@ const router = require("express").Router();
 const sharp = require('sharp');
 const path = require('path')
 const config = require('../config')
-const {resolve} = require("path");
+const { resolve } = require("path");
 
-router.post("/add", async (request, response, next) => {
+router.post("/add", async(request, response, next) => {
 
 
     if (!request.files || Object.keys(request.files).length === 0) {
@@ -38,7 +38,7 @@ router.post("/add", async (request, response, next) => {
     let sampleFile = request.files.file.data;
     let filename = slugtr + ".jpg"
     await sharp(sampleFile)
-        .resize(400, 370).jpeg({quality: 80})
+        .resize(400, 370).jpeg({ quality: 80 })
         .toFile(path.resolve('public/posts/' + filename)).then(() => {
             uploadPath += ["posts", filename].join('/')
         }).catch((err) => {
@@ -46,22 +46,23 @@ router.post("/add", async (request, response, next) => {
         });
 
     const dt = [nametr, cattr, tagtr, posttr, create, slugtr, langtr, group_lang, uploadPath, alttr,
-        nameen, caten, tagen, posten, create, slugen, langen, group_lang, uploadPath, alten]
+        nameen, caten, tagen, posten, create, slugen, langen, group_lang, uploadPath, alten
+    ]
 
     dao.run('insert into posts ("name","cat","tags","post","create","slug","lang","group_lang","image_url","alt") values(?,?,?,?,?,?,?,?,?,?)' +
-        ' ,(?,?,?,?,?,?,?,?,?,?)',
-        dt)
-        .then(async () => {
-            response.json({message: "success", status: 1})
+            ' ,(?,?,?,?,?,?,?,?,?,?)',
+            dt)
+        .then(async() => {
+            response.json({ message: "success", status: 1 })
         })
         .catch((err) => {
-            response.json({message: err, status: 0})
+            response.json({ message: err, status: 0 })
         });
 
 })
 
 
-router.put("/update", async (request, response, next) => {
+router.put("/update", async(request, response, next) => {
 
     if (!request.files || Object.keys(request.files).length === 0) {
         response.status(400).send('No files were uploaded.');
@@ -90,7 +91,7 @@ router.put("/update", async (request, response, next) => {
     let sampleFile = request.files.file.data;
     let filename = slugtr + ".jpg"
     await sharp(sampleFile)
-        .resize(400, 370).jpeg({quality: 80})
+        .resize(400, 370).jpeg({ quality: 80 })
         .toFile(path.resolve('public/posts/' + filename)).then(() => {
             uploadPath += ["posts", filename].join('/')
         }).catch((err) => {
@@ -100,23 +101,21 @@ router.put("/update", async (request, response, next) => {
     // const dt = [nametr, cattr, tagtr, posttr, update, slugtr, uploadPath, alttr,idtr],
     //     dt2=[nameen, caten, tagen, posten, update, slugen, uploadPath, alten,iden]
 
-    await dao.run(`update posts set "name"=? , "cat"=?,"tags"=?,"post"=?,"update"=?,"slug"=?,"image_url"=?,"alt"=? where "id"=?`,
-        [nametr, cattr, tagtr, posttr, update, slugtr, uploadPath, alttr,idtr])
-        .then( async () => {
-            await dao.run('update posts set "name"=? , "cat"=?,"tags"=?,"post"=?,"update"=?,"slug"=?,"image_url"=?,"alt"=? where "id"=?',
-                [nameen, caten, tagen, posten, update, slugen, uploadPath, alten,iden])
-                .then( () => {
+    await dao.run(`update posts set "name"=? , "cat"=?,"tags"=?,"post"=?,"update"=?,"slug"=?,"image_url"=?,"alt"=? where "id"=?`, [nametr, cattr, tagtr, posttr, update, slugtr, uploadPath, alttr, idtr])
+        .then(async() => {
+            await dao.run('update posts set "name"=? , "cat"=?,"tags"=?,"post"=?,"update"=?,"slug"=?,"image_url"=?,"alt"=? where "id"=?', [nameen, caten, tagen, posten, update, slugen, uploadPath, alten, iden])
+                .then(() => {
                     console.log("update")
-                    response.json({message: "success", status: 1})
+                    response.json({ message: "success", status: 1 })
                 })
                 .catch((err) => {
                     console.log("err2")
-                    response.status(500).json({message: err, status: 0})
+                    response.status(500).json({ message: err, status: 0 })
                 });
         })
         .catch((err) => {
             console.log("err1")
-            response.status(500).json({message: err, status: 0})
+            response.status(500).json({ message: err, status: 0 })
         });
 
 
@@ -132,8 +131,8 @@ router.get("/getByName", (request, response, next) => {
 
 router.get("/getByNames", (request, response, next) => {
     const dao = db.getInstance();
-    const {tr,en}=request.query
-    dao.all('SELECT name FROM posts where name!=? and name!=?', [tr,en]).then((data) => {
+    const { tr, en } = request.query
+    dao.all('SELECT name FROM posts where name!=? and name!=?', [tr, en]).then((data) => {
         response.json(data)
     })
 })
@@ -144,14 +143,14 @@ router.get("/getById", (request, response, next) => {
     const query = 'SELECT  posts.* \n' +
         ', cats.id as catid \n' +
         'FROM posts \n' +
-        'inner join cats on cats.id=posts.cat \n'+
+        'inner join cats on cats.id=posts.cat \n' +
         ' where posts.group_lang=?'
-    const {group_lang}=request.query
-    dao.all(query,
-        [group_lang]).then(async (data) => {
-        let g = data.map(async (i) => {
+    const { group_lang } = request.query
+    dao.all(query, [group_lang]).then(async(data) => {
+        let g = data.map(async(i) => {
             const split = i.tags.split(',')
-            let qr = [], temp
+            let qr = [],
+                temp
             split.forEach((i, index) => {
                 qr.push(" id=?")
             })
@@ -176,11 +175,11 @@ router.get("/getAll", (request, response, next) => {
         ', cats.nametr as cattr,cats.nameen as caten\n' +
         'FROM posts \n' +
         'inner join cats on cats.id=posts.cat \n'
-    dao.all(query,
-        []).then(async (data) => {
-        let g = data.map(async (i) => {
+    dao.all(query, []).then(async(data) => {
+        let g = data.map(async(i) => {
             const split = i.tags.split(',')
-            let qr = [], temp
+            let qr = [],
+                temp
             split.forEach((i, index) => {
                 qr.push(" id=?")
             })
@@ -200,9 +199,9 @@ router.get("/getAll", (request, response, next) => {
 })
 router.delete("/delete", (request, response, next) => {
     const dao = db.getInstance();
-    const {id} = request.query;
+    const { id } = request.query;
     dao.run('delete from posts where id=:id', [id]).then((data) => {
-        response.json({status: 1, "message": "success"})
+        response.json({ status: 1, "message": "success" })
     })
 })
 
