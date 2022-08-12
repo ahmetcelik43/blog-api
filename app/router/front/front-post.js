082ac2e1a3b6577d7d3ba9744075da8f68ba5d4e
ahmetcelik43 <ahmet.celik5443@gmail.com>
2022-08-12 22:17:05 +0300

    ok

--- app/router/front/front-post.js
+++ app/router/front/front-post.js
@@ -6,17 +6,30 @@ const router = require("express").Router();
 
 router.get("/getAll", (request, response, next) => {
     const dao = db.getInstance();
-    const query = 'SELECT  posts.* , tags.nametr as tagtr,tags.nameen as tagen\n' +
+    const query = 'SELECT  posts.* \n' +
         ', cats.nametr as cattr,cats.nameen as caten\n' +
         'FROM posts \n' +
-        'left join tags on posts.tags = tags.id \n' +
-        'inner join cats on cats.id=posts.cat \n' +
-        'where tags.id in (posts.tags)\n' +
-        '\n'
+        'inner join cats on cats.id=posts.cat \n'
     dao.all(query,
-        []).then((data) => {
-            console.info(data)
-        response.json(data)
+        []).then(async (data) => {
+        let g = data.map(async (i) => {
+            const split = i.tags.split(',')
+            let qr = [], temp
+            split.forEach((i, index) => {
+                qr.push(" id=?")
+            })
+            let obj = i
+            const q = "select id,nametr,nameen from tags where " + qr.join(' or ')
+            await dao.all(q,
+                split).then((d) => {
+                obj["tagler"] = d
+            })
+            return obj
+        })
+
+        Promise.all(g).then((d) => {
+            response.json(d)
+        })
     })
 })
 
